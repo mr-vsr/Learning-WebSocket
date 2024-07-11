16aebb469aef88c286fc352b4f813482aefb000b
mr-vsr <vikasraivsr630@gmail.com>
2024-07-12 00:55:32 +0530

    implement socket.io in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,36 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io.connect("http://localhost:9000");
+
 function App() {
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    console.log(messageReceived);
+    socket.emit("client-message",{message});
+  }
+
+  useEffect(() => {
+    socket.on("server-message", (data) => {
+      setMessageReceived(data);
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        React app!
-        </div>
+      <div className="App">
+        <input
+          placeholder="Enter Your Message" 
+          onChange={(event)=> setMessage(event.target.value)}
+          />
+        <button onClick={sendMessage}>Send</button>
+        <h1>Message:</h1>
+        {messageReceived}
+      </div>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,25 +1,41 @@
 import http from "http";
 import express from "express";
-import path from "path";
+// import path from "path";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
 
-//socket.io
-io.on('connection', (socket) => {
-    // console.log('a new user has connected', socket.id);
-    socket.on("user-message", message => {
-        // console.log('A new user message arrived ', message);
-        io.emit("message", message); //if any message comes send it to all
-    });
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods:["GET","POST"],
+    }
 });
 
-app.use(express.static(path.resolve("./public")));
+//socket.io
+// io.on('connection', (socket) => {
+//     // console.log('a new user has connected', socket.id);
+//     socket.on("user-message", message => {
+//         // console.log('A new user message arrived ', message);
+//         io.emit("message", message); //if any message comes send it to all
+//     });
+// });
 
-app.get("/", (req, res) => {
-    return res.sendFile("/public/index.html");
+// app.use(express.static(path.resolve("./public")));//serving static file
+
+// app.get("/", (req, res) => {
+//     return res.sendFile("/public/index.html");
+// })
+
+io.on("connection", (socket) => {
+    console.log(`User connected`, socket.id);
+    socket.on("client-message", (data) => {
+        // console.log(data);
+        socket.broadcast.emit("server-message", data.message);
+    })
 })
 
 const PORT = 9000;
